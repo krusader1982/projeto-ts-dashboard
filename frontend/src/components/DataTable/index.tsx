913bd27799fbdedce0e65173b0064542373e47cc
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-10-04 00:33:14 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,49 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Anakin</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>29</td>
+                        <td>21</td>
+                        <td>13556.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Clark Kent</td>
+                        <td>33</td>
+                        <td>28</td>
+                        <td>11125.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Padme</td>
+                        <td>34</td>
+                        <td>20</td>
+                        <td>14562.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/krusader1982" target="_blank" rel="noreferrer">Samuel Xavier</a> e <a href="https://github.com/GreiceCastilho" target="_blank" rel="noreferrer">Greice Castilho</a></p>
+                <p className="text-light"><small><strong>Teste de Software</strong><br />
+                    Projeto de Teste de Software: <a href="https://github.com/krusader1982/projeto-ts-dashboard" target="_blank" rel="noreferrer">Teste de Software</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
