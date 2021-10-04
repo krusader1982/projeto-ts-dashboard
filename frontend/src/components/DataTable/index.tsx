472f76c831805aa33d32b9b2897053b6f2acab57
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-10-04 00:56:02 -0300

    Basic static Charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -40,6 +40,34 @@ function DataTable() {
                         <td>20</td>
                         <td>14562.00</td>
                     </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Anakin</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>29</td>
+                        <td>21</td>
+                        <td>13556.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Clark Kent</td>
+                        <td>33</td>
+                        <td>28</td>
+                        <td>11125.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Padme</td>
+                        <td>34</td>
+                        <td>20</td>
+                        <td>14562.00</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
