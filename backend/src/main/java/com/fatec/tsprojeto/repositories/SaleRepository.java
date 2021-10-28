ef1ea272915e4e58b4511c075c8fb40f1fddd5dd
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-10-28 09:47:10 -0300

    Pagination

---
+++ backend/src/main/java/com/fatec/tsprojeto/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.tsprojeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.tsprojeto.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
