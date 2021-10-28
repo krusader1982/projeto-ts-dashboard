5deb844a165f83b2a933fe3f718eb8558bc1a361
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-10-28 02:54:10 -0300

    Layers

---
+++ backend/src/main/java/com/fatec/tsprojeto/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.tsprojeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.tsprojeto.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
