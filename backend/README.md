66fbd4f30a53b131e237a82a3b35cd6131bd9550
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-10-28 01:19:38 -0300

    Security Config

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Em construção
-
-A implementação do Backend será iniciada a partir da Sprint 3
---
+++ backend/src/main/java/com/fatec/tsprojeto/TsprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.tsprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsprojetoApplication.class, args);
+	}
+
+}
